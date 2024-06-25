5645edce789a270f3cf1653cf0d5360a6d416222
jbgoldman1104 <jbgoldman1104@gmail.com>
2024-06-25 20:38:14 +0000

    Updated Wallets

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const { getVaultToken, generateWallet } = require('./utils');
+require('dotenv').config({path: '.env'});
+const { getVaultToken, generateWallet, sendCoins, sendNXQs } = require('./utils');
+const fs = require('fs');
+const { Web3, HttpProvider } = require("web3");
+const { isAddress } = require("web3-validator")
+
+const web3 = new Web3(new HttpProvider(process.env.RPC_URL));
 
 require('dotenv').config({path: '.env'});
 
@@ -14,12 +20,131 @@ const generateWallets = async ( count ) => {
   
 }
 
+const readWallets = () => {
+  const wallets = fs.readFileSync(`./${process.env.WALLET_FILE}`).toString();
+  
+  return wallets.split('\n');
+}
 
-generateWallets(3990).then(()=>{
+const disperseBNB = async (from, wallets, eachAmount, TOKEN) => {
+  try {
+    
+    console.log('Amount >> ', eachAmount);
 
-}).catch(error => {
+    for ( let i = 0 ; i < wallets.length; i++ ) {
+      console.log('Wallet ', i);
+      await sendCoins(from, wallets[i], eachAmount, TOKEN);
+    }
+  } catch ( error ) {
+    console.log(error);
+  }
+}
 
-})
+const disperseNXQ = async (from, wallets, eachAmount, TOKEN) => {
+  try {
+    
+    console.log('Amount >> ', eachAmount);
+
+    for ( let i = 0 ; i < wallets.length; i++ ) {
+      console.log('Wallet ', i, wallets[i]);
+      const amount = getRandomInteger( 1, eachAmount)
+      const result = await sendNXQs(from, wallets[i], amount * 10**15, TOKEN);
+      if ( result == false ) {
+        await sleep(300);
+        continue;
+      }
+      await sleep(getRandomInteger(3600, 7200));
+    }
+  } catch ( error ) {
+    console.log(error);
+  }
+}
+
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+const bnbBallance = 120 * 10 ** 16;
+const eachbnbAmount = Math.floor(Number(bnbBallance) / 400);
+
+const eachNxqAmount = 10;
+
+const run_random_disperse = async() => {
+  const wallets = readWallets();
+  const TOKEN = await getVaultToken();
+  const idxs = new Array(400).fill(0);
+  // await disperseBNB(process.env.MAINTENANCE_WALLET, wallets.slice(10, 400), eachbnbAmount, TOKEN)
+  // await disperseNXQ(process.env.MAINTENANCE_WALLET, wallets.slice(1, 400), eachNxqAmount, TOKEN)
+
+  while ( true ) {
+    let selectedId = -1;
+    for ( let tries = 5; tries > 0 ; tries-- ) {
+      const id = getRandomInteger(0, 399);
+      if ( idxs[id] < 9 ) {
+        selectedId = id;
+        break;
+      }
+    }
+
+    if ( selectedId == -1 ) {
+      for ( selectedId = 0; selectedId < 400; selectedId++ ) {
+        if (idxs[selectedId] < 9) {
+          break;
+        }
+      }
+    }
+
+    if ( selectedId == 400 ) {
+      break;
+    }
+
+    const targetId = 400 + selectedId * 9 + idxs[selectedId];
+    console.log(`SelectedId ${selectedId}, Target = ${targetId}, wallet = ${wallets[targetId]}`);
+
+    idxs[selectedId]++;
+
+    await disperseNXQ(wallets[selectedId], wallets.slice(targetId, targetId + 1), eachNxqAmount, TOKEN)
+    await sleep(getRandomInteger(3600, 7200));
+  }
+}
+
+const run_random_transfer = async() => {
+  const wallets = readWallets();
+  const TOKEN = await getVaultToken();
+  const eachNxqAmount = 10;
+  for (let tries = 0; tries < 8000; tries ++) {
+    let f_id = getRandomInteger(0, 399);
+    let t_id = getRandomInteger(400, 3999);
+
+    console.log(`Transfer from ${f_id}:${wallets[f_id]}, to = ${t_id}:${wallets[t_id]}`);
+
+    await disperseNXQ(wallets[f_id], wallets.slice(t_id, t_id + 1), eachNxqAmount, TOKEN)
+    await sleep(getRandomInteger(3600, 7200));
+  }
+}
+
+const run_simple_disperse = async() => {
+  const wallets = readWallets();
+  const TOKEN = await getVaultToken();
+  const eachNxqAmount = 500;
+  await disperseNXQ(process.env.MAINTENANCE_WALLET, wallets.slice(8, 400), eachNxqAmount, TOKEN)
+}
+
+
+
+run_random_transfer().then(()=>{}).catch(error=>{});
+
+
+// generateWallets(3990).then(()=>{
+
+// }).catch(error => {
+
+// })
 
 
 
--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,11 @@
 const { default: axios } = require("axios");
 const fs = require('fs');
 
+const { Web3, HttpProvider } = require("web3");
+const web3 = new Web3(new HttpProvider(process.env.RPC_URL));
+const NXQToken_ABI = require('../abi/NXQToken.json');
+const nxqContract = new web3.eth.Contract(NXQToken_ABI, process.env.NXQ_CONTRACT_ADDRESS);
+
 exports.getVaultToken = async () => {
     try {
         const response = await axios.post(`http://127.0.0.1:8200/v1/auth/userpass/login/${process.env.VAULT_USER}`, 
@@ -25,23 +30,15 @@ exports.getVaultToken = async () => {
     }
 }
 
-exports.signTransaction = async (tx) => {
+exports.signTransaction = async (tx, TOKEN) => {
     try {
         
         const {from, to, data, gas, gasPrice, nonce, value} = tx;
-        // console.log({
-        //     data,
-        //     gas,
-        //     gasPrice,
-        //     nonce: "0x" + nonce.toString(16),
-        //     to: to.toLowerCase(),
-        //     value
-        // });
-        const TOKEN = await this.getVaultToken();
-        // console.log("TOKEN >>>", TOKEN);
+
+        console.log(tx, TOKEN, process.env.CHAIN_ID);
         const response = await axios.post(`http://localhost:8200/v1/ethereum/accounts/${from.toLowerCase()}/sign`, 
             {
-                chainId: 7002,
+                chainId: process.env.CHAIN_ID,
                 data : data ? data : "0x00",
                 gas,
                 gasPrice,
@@ -93,4 +90,80 @@ exports.generateWallet = async (TOKEN) => {
     }
 }
 
+exports.sendCoins = async(account, to, amount, TOKEN) => {
+    try {
+      const nonce = await web3.eth.getTransactionCount(account, 'latest');
+      const gasAmount = await web3.eth.estimateGas({
+        from: account,
+        to: to,
+        value: amount,
+        data: "0x00"
+      });
+  
+      console.log('sendCoins gasAmount >>> ', gasAmount);
+  
+      const signedTx = await this.signTransaction({
+        from: account,
+        to: to,
+        value: amount,
+        gas: Number(gasAmount),
+        gasPrice: 1000000000,
+        data: "0x00",
+        nonce: nonce
+      }, TOKEN);
+  
+      if (!signedTx) {
+        console.log('Sign Transaction: Failed');
+        return false;
+      }
+  
+      const tx = await web3.eth.sendSignedTransaction(signedTx.signed_transaction);
+
+      console.log('Send Coins Tx Hash: ', tx.transactionHash);
+  
+      return true;
+    } catch (e) {
+      console.error(e);
+    }
+    return false;
+  }
+
+exports.sendNXQs = async(account, to, amount, TOKEN) => {
+    try {
+
+    //   const balance = Number(await nxqContract.methods.balanceOf(to).call());  
+    //   if ( balance > 0 ) {
+    //     console.log('SKIP >> ', to, balance);        
+    //     return false;
+    //   }
+      const nonce = await web3.eth.getTransactionCount(account, 'latest');
+      const gasAmount = await nxqContract.methods.transfer(to, amount).estimateGas({ from: account });
+      
+      console.log('sendNXQs gasAmount >>> ', gasAmount);
+
+      let data = await nxqContract.methods.transfer(to, amount).encodeABI();
+  
+      const signedTx = await this.signTransaction({
+        from: account,
+        to: process.env.NXQ_CONTRACT_ADDRESS,
+        gas: Number(gasAmount),
+        gasPrice: 1000000000,
+        data,
+        nonce: nonce
+      }, TOKEN);
+  
+      if (!signedTx) {
+        return false;
+      }
+  
+      let tx = await web3.eth.sendSignedTransaction(signedTx.signed_transaction);
+
+      console.log('Send NXQs Tx Hash: ', tx.transactionHash);
+  
+      return true;
+    } catch (e) {
+      console.error(e);
+    }
+    return false;
+}
 
